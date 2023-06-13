bf23b4a56266ba4ab17a69d9b815d7d4c4c5a898
Hameedullah Asadi <hameedasade20@gmail.com>
2023-06-13 14:07:12 -0700

    created method

--- TwentyOneGameConsoleApp/TwentyOneGameConsoleApp/Card.cs
+++ TwentyOneGameConsoleApp/TwentyOneGameConsoleApp/Card.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,17 @@ namespace TwentyOneGameConsoleApp
         public string Suit { get; set; }
         public string Face { get; set; }
 
+
+
+
+
+        public class car
+        {
+            string color = "red";
+            string Make = "Toyota";
+            String Model = "Corolla";
+
+        }
 
 
 
--- TwentyOneGameConsoleApp/TwentyOneGameConsoleApp/Program.cs
+++ TwentyOneGameConsoleApp/TwentyOneGameConsoleApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +13,8 @@ namespace TwentyOneGameConsoleApp
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck(); 
+            Deck deck = new Deck();
+            deck = Shuffling(deck);
 
             foreach (Card card in deck.Cards)
             {
@@ -18,24 +22,30 @@ namespace TwentyOneGameConsoleApp
             }
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
-            //deck.Cards = new List<Card>();
-            //Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit);
-            //Console.ReadLine();
-
-
-
-
-
-
-
-
-            //Card cardone = new Card();
-            //cardone.face = "queens";
-            //cardone.suit = "spades";
-
-            //Console.WriteLine(cardone.Face + " of " + cardone.Suit);
-            //Console.ReadLine();
-
         }
+        public static Deck Shuffling(Deck deck)
+        {
+            List<Card> TempList = new List<Card>();
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                TempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+            deck.Cards = TempList;
+        }
+        //deck.Cards = new List<Card>();
+        //Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit);
+        //Console.ReadLine();
+
+        //Card cardone = new Card();
+        //cardone.face = "queens";
+        //cardone.suit = "spades";
+
+        //Console.WriteLine(cardone.Face + " of " + cardone.Suit);
+        //Console.ReadLine();
     }
-}
+    }
+
