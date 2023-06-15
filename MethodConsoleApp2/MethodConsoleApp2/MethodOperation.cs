86aec90ab7ac447885705cd7bfa2da007a61c7c2
Hameedullah Asadi <hameedasade20@gmail.com>
2023-06-15 11:07:52 -0700

    Created a console app that takes two int data type input and returns the mutlipliction output. It also include an exception handling-try/catch to catch if user does not pick the second number.

---
+++ MethodConsoleApp2/MethodConsoleApp2/MethodOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodConsoleApp2
+{
+    public class MethodOperation
+    {
+        public int MathOperations (int i, int j = 1)
+        {
+            int result = i * j;
+            return result;
+        }
+
+    }
+}
---
+++ MethodConsoleApp2/MethodConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MethodOperation mathOp = new MethodOperation();
+            {
+                Console.WriteLine("Please pick a number:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please pick a second number:");
+                try
+                {
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    int result = mathOp.MathOperations(num1, num2);
+                    Console.WriteLine(num1 + " x " + num2 + " = " + result);
+                }
+                catch
+                {
+                    int result = mathOp.MathOperations(num1);
+                    Console.WriteLine(num1 + " x defualt 1 + = " + result);
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
