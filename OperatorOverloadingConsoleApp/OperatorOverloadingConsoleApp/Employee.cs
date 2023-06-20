29f6edffd059b6afc356afb241430909a3e24942
Hameedullah Asadi <hameedasade20@gmail.com>
2023-06-20 11:43:20 -0700

    worked on operator overloading

---
+++ OperatorOverloadingConsoleApp/OperatorOverloadingConsoleApp/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingConsoleApp
+{
+    public class Employee
+    {
+        public int id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public static Employee operator ==(Employee employee1, Employee employee2)
+        {
+            Employee employeeResult = new Employee(employee1.id == employee2.id);
+            return employeeResult;
+        }
+
+    }
+}
---
+++ OperatorOverloadingConsoleApp/OperatorOverloadingConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  First object of Employee type
+            Employee employee1 = new Employee();    //  Object Instantiation 
+            employee1.id = 5578;
+            employee1.FirstName = "Hameedullah";
+            employee1.LastName = "Asadi";
+
+
+            //  Second Object of Employee type
+            Employee employee2 = new Employee();    //  Object Instantiation 
+            employee2.id = 6678;
+            employee2.FirstName = "Mike";
+            employee2.LastName = "Smith";
+
+
+            Employee employeeResult = employee1 == employee2;
+            Console.ReadLine();
+        }
+    }
+}
