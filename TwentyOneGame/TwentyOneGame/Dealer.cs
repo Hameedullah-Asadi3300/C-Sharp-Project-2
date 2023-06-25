35d722c8c7096b6611f21005ebafacae3910d97f
Hameedullah Asadi <hameedasade20@gmail.com>
2023-06-25 14:00:17 -0700

    Worked on DateTime object

--- TwentyOneGame/TwentyOneGame/Dealer.cs
+++ TwentyOneGame/TwentyOneGame/Dealer.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOneGame
 {
     public class Dealer
     {
-        
+        public string Name { get; set; }
+        public Deck Deck { get; set; }
+        public int Balance { get; set; }
+
+        //  The log statement inside this method keeps log of ths method in a seperate text file
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First());
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");  //  Here the Deck/Cards.First().ToString() is logged
+            Console.WriteLine(card);
+            //The following statement cleans up the memory after it get done the job
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Hameed\Logs\\log.txt", true))
+            {
+                file.WriteLine(DateTime.Now);   //  By using DateTime object in this method, the logs are filed with the current time. It makes sure when something happens.
+                file.WriteLine(card);
+            }
+            Deck.Cards.RemoveAt(0);
+
+
+
+
+        }
     }
 }
--- TwentyOneGame/TwentyOneGame/Program.cs
+++ TwentyOneGame/TwentyOneGame/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;    //  In order to use login we use System.IO namespace becuase the file text exists here.
 
 namespace TwentyOneGame
 {
@@ -11,8 +12,29 @@ namespace TwentyOneGame
     {
         static void Main(string[] args)
         {
+            DateTime birthYear = new DateTime(1995, 05, 27, 08, 48, 13); //  The DateTime object has six parameters of year, month, day, hours, minutes, and seconds
+            //Second DateTime object for timespan
 
-          
+            DateTime yearOfGraduation = new DateTime(2015, 12, 22, 2, 29, 45);
+            TimeSpan ageAtGraduation = yearOfGraduation - yearOfBirth;
+            ageAtGraduation.Duration();
+            //  The timespan object has a lot of built-in features which do different math operations
+            //  The most useful usage of DateTime object is in logging where all logs are appeared with exact time.
+
+
+
+
+
+
+
+
+            //  The text file/Login created
+            string text = "Here is some text";  
+            File.WriteAllText(@"C:\Users\Hameed\Logs\log.txt", text);   //   The "@" means ingnore the space otherwise we need to give back slash "\\" instead one like so: C:\\Users\\Hameed\\Logs\\log.txt, text
+            //  Logging statements ended
+
+
+
             Console.WriteLine("Welcome to the grand Hotel and Casino. Let's start with telling your name:");
             string playerName = (Console.ReadLine());
             Console.WriteLine("And how much money did you bring today?");
